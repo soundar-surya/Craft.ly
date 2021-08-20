f35beddbf199e9c4ab39f4a54473b80fbf76b2cb
soundar-surya <bittergourd76@gmail.com>
2021-08-20 18:14:03 +0530

    Implemented GoogleSheets Integration

--- client/src/components/dashboard/mainComponents/tables/renderGSheets.js
+++ client/src/components/dashboard/mainComponents/tables/renderGSheets.js
@@ -2,7 +2,7 @@ export default function GSheets({state}) {
     
     const renderContent = () => (state.map(({gs: {url}}, i) => 
         (<div >
-            <iframe src={url} title="gSheet" style={{height: '70vh', width: '100%', border: 'none', margin: i > 0 ? '5vh 0 0 0' : null}}/>
+            <iframe src={url} title="gSheet" style={{height: '85vh', width: '100%', border: 'none', margin: i > 0 ? '5vh 0 0 0' : null}}/>
         </div>)))
 
     
